b4cd36068b5b0dc23bdb9aca86578edc1bc7bb2e
ashique1213 <muhammedashique1213@gmail.com>
2026-07-03 18:04:51 +0530

    feat(celery): add retries and robust logging to email tasks

--- Backend/classsphere/authentication/tasks.py
+++ Backend/classsphere/authentication/tasks.py
@@ -1,11 +1,19 @@
 # authentication/task.py
 from celery import shared_task # type: ignore
 from django.core.mail import send_mail
+from celery.utils.log import get_task_logger
 
-@shared_task
-def send_otp_email_task(email, username, otp_code):
+logger = get_task_logger(__name__)
+
+@shared_task(bind=True, max_retries=3)
+def send_otp_email_task(self, email, username, otp_code):
     """Sends OTP email with user details asynchronously."""
     subject = "Verify Your Email - OTP Code"
     message = f"Hello {username},\n\nYour OTP code is: {otp_code}. It will expire in 1 minutes.\n\nThank you!"
     
-    send_mail(subject, message, "no-reply@classsphere.com", [email])
+    try:
+        send_mail(subject, message, "no-reply@classsphere.com", [email])
+        logger.info(f"OTP email sent successfully to {email}")
+    except Exception as exc:
+        logger.error(f"Failed to send OTP email to {email}: {exc}")
+        raise self.retry(exc=exc, countdown=60)
